33f6f487a1577a951c8212431dc5a3c2a1fed6be
sammotaroki <sammotaroki@gmail.com>
2023-06-20 23:23:40 -0700

    prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ * starting with 1 and 2, followed by a new line
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int c;
+	long int n1, n2, fn;
+
+	n1 = 1;
+	n2 = 2;
+	printf("%ld, %ld", n1, n2);
+	for (c = 0; c < 48; c++)
+	{
+		fn = n1 + n2;
+		printf(", %ld", fn);
+		n1 = n2;
+		n2 = fn;
+	}
+	printf("\n");
+	return (0);
+}
