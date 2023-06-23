409e480a6547b18df62d703360c3dcfb4f59edd2
sammotaroki <sammotaroki@gmail.com>
2023-06-22 23:30:04 -0700

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Tests a function that prints if zero is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
