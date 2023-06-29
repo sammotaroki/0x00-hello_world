ed511a09858550aa6c592c393b34058c0e422805
sammotaroki <sammotaroki@gmail.com>
2023-06-29 01:16:19 -0700

    Changing lowercase to uppercase letters

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
+
+char *string_toupper(char *c)
+{
+	int counter = 0;
+
+	while (*(c + counter) != '\0')
+	{
+		if ((*(c + counter) >= 97) && (*(c + counter) <= 122))
+			*(c + counter) = *(c + counter) - 32;
+		counter++;
+	}
+
+	return (c);
+}
