3aa72a6b4a0c38bb53e72c2448fbe84d569be915
sammotaroki <sammotaroki@gmail.com>
2023-06-29 18:09:36 -0700

    Fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,15 +8,15 @@
 char *leet(char *c)
 {
 	int counter = 0, i;
-	int lower_letters[] = {97, 101, 111, 116, 108};
-	int upper_letters[] = {65, 69, 79, 84, 76};
+	int low_letters[] = {97, 101, 111, 116, 108};
+	int upp_letters[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
 
 	while (*(c + counter) != '\0')
 	{
 		for (i = 0; i < 5; i++)
 		{
-			if (*(c + counter) == lower_letters[i] || *(c + counter) == upper_letters[i])
+			if (*(c + counter) == low_letters[i] || *(c + counter) == upp_letters[i])
 			{
 				*(c + counter) = numbers[i];
 				break;
